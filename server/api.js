0080ed54e18e9581550cd0401bb36133b7c1262d
emd0008 <emd0008@uah.edu>
2017-11-01 09:43:58 -0500

    Added js files

--- server/api.js
+++ server/api.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const products_ctrl_1 = require("./controllers/products.ctrl");
 const router = express.Router();
+router
+    .use("/products", products_ctrl_1.default);
 exports.default = router;
