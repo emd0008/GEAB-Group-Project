39a51503a38d9003c4761aaf1f71c42607410642
Aparajita Chakrabarty <aparajitachakrabarty@Aparajitas-MacBook-Air.local>
2017-11-02 13:49:13 -0500

    a

--- client/factories.js
+++ client/factories.js
@@ -1,2 +1,27 @@
 angular
-    .module("MyNewPet.factories", []);
+    .module("MyNewPet.factories", [])
+    .factory("Product", [
+        "$resource",
+        function($resource) {
+            return $resource(
+                "/api/products/:id",
+                {
+                    id: "@id"
+                },
+                {
+                    queryByCategory: {
+                        method: "GET",
+                        url: "/api/products/category/:categoryid",
+                        isArray: true
+                    },
+                    queryBySubCategory: {
+                        method: "GET",
+                        url: "/api/products/category/subcategory/:subcategoryid",
+                        isArray: true
+                    }
+                }
+            )
+        }
+    ])
+    .factory
+    ;
