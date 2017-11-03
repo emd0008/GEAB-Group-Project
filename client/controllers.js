6014210822b8613dd71ae066d8d66293dcd549a6
Aubrey Squires <aubreysquires@Aubreys-MacBook-Pro.local>
2017-11-03 11:53:09 -0500

    Updated sub cat and products

--- client/controllers.js
+++ client/controllers.js
@@ -1,123 +1,122 @@
 angular
-  .module("MyNewPet.controllers", ["ngRoute"])
-  .controller("AnimalsController", [
-    "$scope",
-    "$location",
-    "$resource",
-    "Animals",
-    function($scope, $location, $resource, Animals) {
-      function getAnimals() {
-        $scope.animals = Animals.query();
-      }
-    }
-  ])
-  .controller("SingleAnimalController", [
-    "$scope",
-    "$location",
-    "$resource",
-    function($scope, $location, $resource) {}
-  ])
-  .controller("ProductsController", [
-    "$scope",
-    "$location",
-    "$resource",
-    "Products",
-    function($scope, $location, $resource, Products) {
-      function getProducts() {
-        $scope.products = Products.query();
-      }
-      getProducts();
-    }
-  ])
-  .controller("SingleProductController", [
-    "$scope",
-    "$location",
-    "$resource",
-    "$routeParams",
-    "SingleProduct",
-    function($scope, $location, $resource, $routeParams, SingleProduct) {
-      $scope.product = SingleProduct.get({ id: $routeParams.id });
-    }
-  ])
-  .controller("SubCatController", ["$scope", "$location", "$routeParams", "Products", function($scope, $location, $resource, $routeParams, SubCategory) {
-    // You'll need the Product(s)  factory pulled in here
-    // this controller is in charge of getting a list of all products with a given subcategory
-    $scope.products=Products.queryBySubcategory({subcategoryid: $routeParams.id});
-  }
-])
-  .controller("ApplyController", [
-    "$scope",
-    "$location",
-    "$resource",
-    function($scope, $location, $resource) {}
-  ])
-  .controller("ContactController", [
-    "$scope",
-    "ContactForm",
-    "$location",
-    function($scope, ContactForm, $location) {
-      $scope.send = function() {
-        let contact = new ContactForm({
-          from: $scope.from,
-          subject: $scope.subject,
-          message: $scope.message
-        });
+    .module("MyNewPet.controllers", ["ngRoute"])
+    // .controller("AnimalsController", [
+    //     "$scope",
+    //     "$location",
+    //     "$resource",
+    //     "Animals",
+    //     function($scope, $location, $resource, Animals) {
+    //         function getAnimals() {
+    //             $scope.animals = Animals.query();
+    //         }
+    //     }
+    // ])
+    // .controller("SingleAnimalController", [
+    //     "$scope",
+    //     "$location",
+    //     "$resource",
+    //     function($scope, $location, $resource) {}
+    // ])
+    .controller("ProductsController", [
+        "$scope",
+        "$location",
+        "$resource",
+        "Products",
+        function($scope, $location, $resource, Products) {
+            function getProducts() {
+                $scope.products = Products.query();
+            }
+            getProducts();
+        }
+    ])
+    .controller("SingleProductController", [
+        "$scope",
+        "$location",
+        "$resource",
+        "$routeParams",
+        "Products",
+        function($scope, $location, $resource, $routeParams, Products) {
+            $scope.product = Products.get({ id: $routeParams.id });
+        }
+    ])
+    .controller("SubCatController", ["$scope", "$location", "$routeParams", "Products", function($scope, $location, $resource, $routeParams, SubCategory) {
+        // You'll need the Product(s)  factory pulled in here
+        // this controller is in charge of getting a list of all products with a given subcategory
+        $scope.products = Products.queryBySubcategory({ subcategoryid: $routeParams.id });
+    }])
+    // .controller("ApplyController", [
+    //     "$scope",
+    //     "$location",
+    //     "$resource",
+    //     function($scope, $location, $resource) {}
+    // ])
+    .controller("ContactController", [
+        "$scope",
+        "ContactForm",
+        "$location",
+        function($scope, ContactForm, $location) {
+            $scope.send = function() {
+                let contact = new ContactForm({
+                    from: $scope.from,
+                    subject: $scope.subject,
+                    message: $scope.message
+                });
 
-        //makes a POST request to /api/contactform with a body with properties from and message
-        contact.$save(
-          function() {
-            alert(
-              "Thank you for your message! We will get back to you shortly."
-            );
-          },
-          function(err) {
-            console.log(err);
-          }
-        );
-      };
-    }
-  ])
-  .controller("SummaryController", [
-    "$scope",
-    "$resource",
-    function($scope, $resource) {}
-  ])
-  .controller("CheckoutController", [
-    "$scope",
-    "$resource",
-    function($scope, $resource) {}
-  ])
-  .controller("PaymentController", [
-    "$scope",
-    "Payment",
-    function($scope, Payment) {
-      let elements = stripe.elements();
-      let card = elements.create("card");
-      card.mount("#card-number");
-      $scope.process = function() {
-        stripe.createToken(card).then(result => {
-          if (result.error) {
-            $scope.error = result.error.message;
-          } else {
-            let p = new Payment({
-              token: result.token.id,
-              amount: $scope.amount
-            });
+                //makes a POST request to /api/contactform with a body with properties from and message
+                contact.$save(
+                    function() {
+                        alert(
+                            "Thank you for your message! We will get back to you shortly."
+                        );
+                    },
+                    function(err) {
+                        console.log(err);
+                    }
+                );
+            };
+        }
+    ])
+    // .controller("SummaryController", [
+    //     "$scope",
+    //     "$resource",
+    //     function($scope, $resource) {}
+    // ])
+    // .controller("CheckoutController", [
+    //     "$scope",
+    //     "$resource",
+    //     function($scope, $resource) {}
+    // ])
+    .controller("PaymentController", [
+        "$scope",
+        "Payment",
+        function($scope, Payment) {
+            let elements = stripe.elements();
+            let card = elements.create("card");
+            card.mount("#card-number");
+            $scope.process = function() {
+                stripe.createToken(card).then(result => {
+                    if (result.error) {
+                        $scope.error = result.error.message;
+                    } else {
+                        let p = new Payment({
+                            token: result.token.id,
+                            amount: $scope.amount
+                        });
 
-            p.$save(
-              function() {
-                $location.path("/");
-              },
-              function(err) {
-                $scope.error = err.data;
-              }
-            );
-          }
-        });
-      };
-    }
-  ])
-  .controller("LoginController", ["$scope", function($scope) {}]);
+                        p.$save(
+                            function() {
+                                $location.path("/");
+                            },
+                            function(err) {
+                                $scope.error = err.data;
+                            }
+                        );
+                    }
+                });
+            };
+        }
+    ])
+    .controller("LoginController", ["$scope", function($scope) {}]);
 
 
-  $scope.products = Products.queryBySubcategory({ subcategoryid: $routeParams.id });  
+// $scope.products = Products.queryBySubcategory({ subcategoryid: $routeParams.id });
--- client/factories.js
+++ client/factories.js
@@ -1,29 +1,25 @@
 angular
-  .module("MyNewPet.factories", [])
-  .factory("ContactForm", [
-    "$resource",
-    function($resource) {
-      return $resource("/api/contactform/:id", { id: "@id" });
-    }
-  ])
-  .factory("Products", [
-    "$resource",
-    function($resource) {
-      return $resource(
-        "/api/products/:id",
-        { id: "@id" },
-        {
-          queryByCategory: {
-            method: "GET",
-            url: "/api/products/category/:categoryid",
-            isArray: true
-          },
-          queryBySubcategory: {
-            method: "GET",
-            url: "/api/products/subcategory/:subcategoryid",
-            isArray: true
-          }
+    .module("MyNewPet.factories", [])
+    .factory("ContactForm", [
+        "$resource",
+        function($resource) {
+            return $resource("/api/contactform/:id", { id: "@id" });
         }
-      );
-    }
-  ]);
+    ])
+    .factory("Products", [
+        "$resource",
+        function($resource) {
+            return $resource("/api/products/:id", { id: "@id" }, {
+                queryByCategory: {
+                    method: "GET",
+                    url: "/api/products/category/:categoryid",
+                    isArray: true
+                },
+                queryBySubcategory: {
+                    method: "GET",
+                    url: "/api/products/subcategory/:subcategoryid",
+                    isArray: true
+                }
+            });
+        }
+    ]);
