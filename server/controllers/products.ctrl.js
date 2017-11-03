b7ff20ce837f55726ecb1d502e183f634ad903f2
Aubrey Squires <aubreysquires@Aubreys-MacBook-Pro.local>
2017-11-03 14:13:29 -0500

    changes

--- server/controllers/products.ctrl.js
+++ server/controllers/products.ctrl.js
@@ -22,32 +22,32 @@ router.get("/", (req, res) => {
 //         res.sendStatus(500);
 //     });
 // });
-router.route("/category/:CatID").get((req, res) => {
-    procedures.productsByCat(req.params.CatID)
-        .then(products => {
-        res.send(products);
-    })
-        .catch(err => {
-        res.sendStatus(500);
-    });
-});
-router.route("/subcategory/:SubCatID").get((req, res) => {
-    procedures.productsBySubCat(req.params.SubCatID)
-        .then(products => {
-        res.send(products);
-    })
-        .catch(err => {
-        res.sendStatus(500);
-    });
-});
-router.route("/:id").get((req, res) => {
-    procedures.product(req.params.id)
-        .then(product => {
-        res.send(product);
-    })
-        .catch(err => {
-        console.log(err);
-        res.sendStatus(500);
-    });
-});
+// router.route("/category/:CatID").get((req, res) => {
+//     procedures.productsByCat(req.params.CatID)
+//     .then(products => {
+//         res.send(products);
+//     })
+//     .catch(err => {
+//         res.sendStatus(500);
+//     });
+// });
+// router.route("/subcategory/:SubCatID").get((req, res) => {
+//     procedures.productsBySubCat(req.params.SubCatID)
+//     .then(products => {
+//         res.send(products);
+//     })
+//     .catch(err => {
+//         res.sendStatus(500);
+//     });
+// });
+// router.route("/:id").get((req, res) => {
+//     procedures.product(req.params.id)
+//     .then(product => {
+//         res.send(product);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.sendStatus(500);
+//     });
+// });
 exports.default = router;
