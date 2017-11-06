d12b90f20a63671323aa9123c054de6a40144433
Aparajita Chakrabarty <aparajitachakrabarty@Aparajitas-MacBook-Air.local>
2017-11-06 11:11:43 -0600

    more changes

--- server/procedures/products.proc.js
+++ server/procedures/products.proc.js
@@ -1,22 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_1 = require("../config/db");
-function getDogToys() {
-    return db_1.rows("GetAllDogToys");
-}
-exports.getDogToys = getDogToys;
-function getAllDogs() {
-    return db_1.rows("GetAllDogs");
-}
-exports.getAllDogs = getAllDogs;
-function getAllCats() {
-    return db_1.row("GetAllCats");
-}
-exports.getAllCats = getAllCats;
-function productsByCat(CatID) {
-    return db_1.rows("ProductsByCategory", [CatID]);
-}
-exports.productsByCat = productsByCat;
 function productsBySubCat(SubCatID) {
     return db_1.rows("ProductsBySubcategory", [SubCatID]);
 }
