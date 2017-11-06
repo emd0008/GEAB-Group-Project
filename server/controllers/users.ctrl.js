0fc4a307829475da17c7f36afc789b4c4bf42647
Aparajita Chakrabarty <aparajitachakrabarty@Aparajitas-MacBook-Air.local>
2017-11-06 10:56:53 -0600

    stuff

--- server/controllers/users.ctrl.js
+++ server/controllers/users.ctrl.js
@@ -1,1 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const procedures = require("../procedures/users.proc");
+let router = express.Router();
+router.get('/', (req, res) => {
+    procedures.all()
+        .then((users) => {
+        res.send(users);
+    }, (err) => {
+        res.status(500).send(err);
+    });
+});
+exports.default = router;
--- server/procedures/products.proc.ts
+++ server/procedures/products.proc.ts
@@ -1,21 +1,5 @@
 import {row, rows} from "../config/db";
 
-export function getDogToys(): Promise<Array<models.IProduct>>{
-    return rows("GetAllDogToys");
-}
-
-export function getAllDogs(): Promise<Array<models.IProduct>> {
-    return rows("GetAllDogs");
-}
-
-export function getAllCats(): Promise<Array<models.IProduct>>{
-    return row("GetAllCats");
-}
-
-export function productsByCat(CatID: number): Promise<Array<models.IProduct>>{
-    return rows("ProductsByCategory", [CatID]);
-}
-
 export function productsBySubCat(SubCatID: number): Promise<Array<models.IProduct>>{
     return rows("ProductsBySubcategory", [SubCatID]);
 }
