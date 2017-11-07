8525d17e36a86aa7884a369fa55f63af5863365e
emd0008 <emd0008@uah.edu>
2017-11-07 11:44:40 -0600

    password/user update

--- server/controllers/users.ctrl.ts
+++ server/controllers/users.ctrl.ts
@@ -1,19 +1,97 @@
 import * as express from 'express';
+import * as passport from 'passport';
 import * as procedures from '../procedures/users.proc';
 import * as auth from '../middleware/auth.mw';
+import * as utils from '../utils';
 
 let router = express.Router();
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err: any, user: models.IUser, info: any) => {
+        if(err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        if(!user){
+            return res.status(401).send(info);
+        }
+        req.logIn(user, (err) => {
+            if(err){
+                console.log(err);
+                return res.sendStatus(500);
+            }else{
+                delete user.password;
+                return res.send(user);
+            }
+        });
+    })(req, res, next);
+});
 
 router.get('/', (req, res) => {
     procedures.all()
     .then((users) => {
         res.send(users);
     }, (err) => {
-    
+        console.log(err);
         res.status(500).send(err);
     });
 });
-router.get('/', auth.isLoggedIn, (req, res) => {
 
+router.post('/', (req, res) => {
+    utils.encryptPassword(req.body.password)
+    .then((hash) => {
+        return procedures.createUser(req.body.email, hash);
+    }).then((id) => {
+        res.send(id);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
 });
+
+router.all('*', auth.isLoggedIn);
+
+router.get('/logout', (req, res) => {
+    if(req.session){
+        req.session.destroy(() => {
+            req.logOut();
+            res.sendStatus(204);
+        });
+    }
+});
+
+router.route('/').get(auth.isAdmin, (req, res) => {
+    procedures.all()
+    .then((users) => {
+        res.send(users);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+}).post(auth.isAdmin, (req, res) => {
+    utils.encryptPassword(req.body.password)
+    .then((hash) => {
+        return procedures.createUser(req.body.email, hash);
+    }).then((id) => {
+        res.status(201).send(id);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+router.get('/me', (req, res) => {
+    res.send(req.user);
+});
+
+router.route(':/id').get(auth.isAdmin, (req, res) => {
+    procedures.read(req.params.id)
+    .then((user) => {
+        res.send(user);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
 export default router;
--- server/procedures/users.proc.ts
+++ server/procedures/users.proc.ts
@@ -1,5 +1,9 @@
 import {row, rows, empty} from '../config/db';
 
+export function createUser(email: string, hash: string):Promise <models.IUser>{
+    return row('CreateUser', [email, hash]);
+}
+
 export function readByEmail(email:string):Promise <models.IUser>{ 
     return row ('GetUserByEmail', [email]);
 } 
