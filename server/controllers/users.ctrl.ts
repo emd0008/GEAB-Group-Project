2703d009c219519db192ade2df0786a35b383726
emd0008 <emd0008@uah.edu>
2017-11-07 12:14:59 -0600

    update

--- server/controllers/users.ctrl.ts
+++ server/controllers/users.ctrl.ts
@@ -6,7 +6,6 @@ import * as utils from '../utils';
 
 let router = express.Router();
 
-<<<<<<< HEAD
 
 router.post('/login', (req, res, next) => {
     // In here, it's our job to kick off authentication
@@ -24,39 +23,11 @@ router.post('/login', (req, res, next) => {
                 console.log(err);
                 return res.sendStatus(500);
             } else {
-=======
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err: any, user: models.IUser, info: any) => {
-        if(err){
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        if(!user){
-            return res.status(401).send(info);
-        }
-        req.logIn(user, (err) => {
-            if(err){
-                console.log(err);
-                return res.sendStatus(500);
-            }else{
->>>>>>> 2643d3daf5fa9a6ea8526fc6e77e9cb5b4706b7d
                 delete user.password;
                 return res.send(user);
             }
         });
     })(req, res, next);
-<<<<<<< HEAD
-});
-
-router.all('*', auth.isLoggedIn);
-// router.get('*', auth.isLoggedIn);
-// router.post('*', auth.isLoggedIn);
-// router.put('*', auth.isLoggedIn);
-// router.delete('*', auth.isLoggedIn);
-
-router.get('/logout', (req, res) => { // actually /api/users/logout
-    if (req.session) {
-=======
 });
 
 router.get('/', (req, res) => {
@@ -85,58 +56,11 @@ router.all('*', auth.isLoggedIn);
 
 router.get('/logout', (req, res) => {
     if(req.session){
->>>>>>> 2643d3daf5fa9a6ea8526fc6e77e9cb5b4706b7d
         req.session.destroy(() => {
             req.logOut();
             res.sendStatus(204);
         });
     }
-<<<<<<< HEAD
-});
-
-router
-    .route('/') // actually /api/users/
-        .get(auth.isAdmin, (req, res) => {
-            procedures.all()
-            .then((users) => {
-                res.send(users);
-            }).catch((err) => {
-                console.log(err);
-                res.sendStatus(500);
-            });
-        })
-        .post(auth.isAdmin, (req, res) => {
-            let u = req.body; // The incoming POST request data (e.g. the new user being created)
-            utils.encryptPassword(u.password)
-            .then((hash) => {
-                return procedures.create(u.email, hash, u.firstname, u.lastname);
-            }).then((id) => {
-                res.status(201).send(id);
-            }).catch((e) => {
-                console.log(e);
-                res.sendStatus(500);
-            });
-        });
-
-
-router.get('/me', (req, res) => {
-    res.send(req.user);
-});
-
-router
-    .route('/:id') // actually /api/users/:id
-        .get(auth.isAdmin, (req, res) => {
-            procedures.read(req.params.id)
-            .then((user) => {
-                res.send(user);
-            }).catch((err) => {
-                console.log(err);
-                res.sendStatus(500);
-            });
-        });
-
-
-=======
 });
 
 router.route('/').get(auth.isAdmin, (req, res) => {
@@ -172,6 +96,5 @@ router.route(':/id').get(auth.isAdmin, (req, res) => {
         res.sendStatus(500);
     });
 });
->>>>>>> 2643d3daf5fa9a6ea8526fc6e77e9cb5b4706b7d
 
 export default router;
