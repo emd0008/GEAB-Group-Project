cc0da07eaad7351ffe397226d6ecf914a6c073c3
Aubrey Squires <aubreysquires@Aubreys-MacBook-Pro.local>
2017-11-08 10:43:58 -0600

    changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,26 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const passport = require("passport");
 const session = require("express-session");
-let MySQLStore = require("express-mysql-session")(session);
+let MySQLStore = require('express-mysql-session')(session);
 const passport_local_1 = require("passport-local");
 const userProc = require("../procedures/users.proc");
 const db_1 = require("./db");
 const utils = require("../utils");
 function configurePassport(app) {
     passport.use(new passport_local_1.Strategy({
-        usernameField: "email",
-        passwordField: "password"
+        usernameField: 'email',
+        passwordField: 'password'
     }, (email, password, done) => {
-<<<<<<< HEAD
         let loginError = 'Invalid Login Credentials';
         userProc.readByEmail(email).then((user) => {
-=======
-        userProc.readByEmail(email).then(user => {
->>>>>>> 776ac61111c9dc3f966576087f141ae23bba7d3c
             if (!user) {
                 return done(null, false, { message: loginError });
             }
-<<<<<<< HEAD
             return utils.checkPassword(password, user.password)
                 .then((matches) => {
                 if (matches) {
@@ -33,13 +28,6 @@ function configurePassport(app) {
                 }
             });
         }).catch((err) => {
-=======
-            if (user.password !== password) {
-                return done(null, false, { message: "Nope!" });
-            }
-            return done(null, user);
-        }, err => {
->>>>>>> 776ac61111c9dc3f966576087f141ae23bba7d3c
             return done(err);
         });
     }));
